dbe11ed09aab401e77b8edd65ab9eb4044a5d753
chris9692 <chris.li@dyna-bi.com>
2018-05-02 19:31:53 -0700

    Update balanced.py

--- balanced.py
+++ balanced.py
@@ -1,14 +1,22 @@
 def balanced_string(s):
-    symbols = {'}':'{', ')': '(', ']':'['}
+    
+    # create a symbos dictionary like {'}':'{', ')': '(', ']':'['}
+    # with closing brackets as keys, and opening brackets as values
+    symbols = dict(zip('}])', '{[(')) 
+    
+    # create a stack to track opening brackets
     stack = []
+    
     for c in s:
         if c in symbols.values():
             stack.append(c)
         elif c in symbols.keys():
-            if len(stack) > 0 and stack.pop() == symbols[c]:
-                continue
-            else:
-                return False
+            if len(stack) > 0 and stack.pop() == symbols[c]: continue
+            return False
+        else:
+            #discard all other characters
+            continue
+            
     return len(stack) == 0
 
 if __name__ == '__main__':
