77ae45b019d5368337b18680739a0c2cfd3816e6
chris9692 <chris.li@dyna-bi.com>
2018-05-02 18:12:39 -0700

    Update permute.py

--- permute.py
+++ permute.py
@@ -3,13 +3,14 @@ def permute(chars, p):
         print(''.join(chars))
     else:
         for i in range(p, len(chars)):
-            temp = chars[i]
-            chars[i] = chars[p]
-            chars[p] = temp
+            
+            #move data at position i to position p
+            chars[i], chars[p] = chars[p], chars[i]
+            
             permute(chars, p + 1)
-            temp = chars[i]
-            chars[i] = chars[p]
-            chars[p] = temp
+            
+            #switch back data at position i and p
+            chars[i], chars[p] = chars[p], chars[i]
 
 if __name__ == '__main__':
     permute(list("cat"), 0)        
