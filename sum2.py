1b11fd92c78d199ba4a62d94bd267d90e247f9a6
chris9692 <chris.li@dyna-bi.com>
2018-05-05 11:19:43 -0700

    Update sum2.py

--- sum2.py
+++ sum2.py
@@ -1,3 +1,4 @@
+
 def two_sum(a, b):
     """
     export all combinations of number pairs, one from each
@@ -12,3 +13,20 @@ def two_sum(a, b):
 
 if __name__ == '__main__':
     two_sum(range(10), range(-9,0))
+
+
+def two_sum2(a, b, s):
+    """
+    export all combinations of number pairs, one from each
+    set, that add up to s
+    param a: set of unique integers
+    param b: set of unique integers
+    param s: the sum 
+    """
+    lookup = {(s-n): n for n in a}
+    for n in b:
+        if lookup.get(n) != None:
+            print(lookup[n], n)
+
+if __name__ == '__main__':
+    two_sum2(range(10), range(-9,0), 1)    
