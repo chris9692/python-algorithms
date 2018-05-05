ac7f9b66bc51aa8e0e9a30d5337b545648fd92a8
chris9692 <chris.li@dyna-bi.com>
2018-05-05 12:16:24 -0700

    Create anysum.py

---
+++ anysum.py
@@ -0,0 +1,27 @@
+def any_sum(s, *args):
+    """
+    export all combinations of numbers, one from each
+    set as given in args, that add up to s
+    param s: the sum 
+    param args: sets of unique integers
+    """
+    if len(args) < 2:
+        return []
+    
+    if len(args) == 2:
+        combinations = []
+        lookup = {(s-n): n for n in args[0]}
+        for n in args[1]:
+            if lookup.get(n) != None:
+                combinations.append([lookup[n], n])
+    else:
+        combinations = []
+        for n in args[-1]:
+            subsum = any_sum(s-n, *args[:-1])
+            combinations.extend([(*x,n) for x in subsum if n not in x])
+            
+    return set(tuple(sorted(c)) for c in combinations)
+        
+
+if __name__ == '__main__':
+    print(any_sum(0, range(10), range(-9,0), range(10)))
