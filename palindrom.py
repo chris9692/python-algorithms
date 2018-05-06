9d6f0fabc2fea378ad6c025c68b66f8006f7740e
chris9692 <chris.li@dyna-bi.com>
2018-05-06 12:13:45 -0700

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,4 +1,5 @@
 
+
 import numpy as np
 
 def _palindrom(s, st, start, end):
@@ -33,7 +34,42 @@ def maxPalindromSubString(s):
             else:
                 _palindrom(s, states, i, j)
             
-    print(np.max(states))
+    return np.max(states)
 
 if __name__ == '__main__':
-    maxPalindromSubString('qeyhgerthejxxxaabcbaaxxxje')
+    assert(maxPalindromSubString('qeyhgerthejxxxaabcbaaxxxje') == 17)
+
+def maxPalindromSubString2(s):
+    """
+    The centers method
+    """
+    if len(s) == 0:
+        return 0
+    
+    # initialize states and set base cases
+    # for strings of len 1, 2, 3
+    centers = {}
+    
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            centers[(i, i + 1)] = 2
+            
+    for i in range(len(s) - 2):
+        if s[i] == s[i + 2]:
+            centers[(i, i + 2)] = 3
+
+    if len(centers) == 0:
+        return 1
+    
+    for c, l in centers.items():
+        for i in range(1, len(s)):
+            if c[0] - i < 0 or c[1] + i >= len(s):
+                break
+            if s[c[0] - i] == s[c[1] + i]:
+                centers[c] += 2
+        
+    return max(centers.values())
+
+if __name__ == '__main__':
+    assert(maxPalindromSubString2('qaabcbaax') == 7)
+    assert(maxPalindromSubString2('qeyejxxxaabcbaaxxxje') == 17)
