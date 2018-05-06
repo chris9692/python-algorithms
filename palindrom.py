cc74796961d817572585ddb163b338e9437dcd3d
chris9692 <chris.li@dyna-bi.com>
2018-05-06 11:51:08 -0700

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -4,10 +4,6 @@ def _palindrom(s, st, start, end):
     if st[start, end] != -1:
         return st[start, end]
     
-    if end - start == 2:
-        st[start, end] = end - start + 1 if s[start] == s[end] else 0
-        return st[start, end]
-
     if s[start] == s[end] and _palindrom(s, st, start + 1, end - 1) > 0:
         st[start, end] = end - start + 1
     else:
@@ -16,10 +12,18 @@ def _palindrom(s, st, start, end):
     return st[start, end]
 
 def maxPalindromSubString(s):
+    
+    # initialize states and set base cases
+    # for strings of len 1, 2, 3
     states = np.ones(len(s)) * -1 + np.eye(len(s)) * 2
+    
     for i in range(len(s) - 1):
         if s[i] == s[i + 1]:
             states[i, i + 1] = 2
+            
+    for i in range(len(s) - 2):
+        if s[i] == s[i + 2]:
+            states[i, i + 2] = 3
             
     for i in range(len(s)-1):            
         for j in range(i+1, len(s)):
@@ -29,3 +33,4 @@ def maxPalindromSubString(s):
                 _palindrom(s, states, i, j)
             
     print(np.max(states))
+    
